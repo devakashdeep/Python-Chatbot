f9bb26a9ad9604cedc1a8213d58636f49746cf29
devakashdeep <akashdeepcse@gmail.com>
2021-08-16 23:05:15 +0530

    update

--- app/backends.py
+++ app/backends.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
+from django.db.models import Q
 from django.contrib.auth.backends import ModelBackend
 
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = User.objects.get(email__iexact=username)
+            user = User.objects.get(Q(email__iexact=username)| Q(username__iexact=username))
         except User.DoesNotExist:
             return None
 
